2c5aed1b56f19372090a8ab3859d501791dbd636
Astromaut <38085698+Astromaut@users.noreply.github.com>
2026-02-24 16:42:11 +0000

    calendar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 """
-Doplnok davky pre UVN RK
+Doplnok dávky rádioterapie pre ÚVN Ružomberok
 
 (c) Radoslav Paučo
 """
@@ -7,37 +7,38 @@ Doplnok davky pre UVN RK
 import streamlit as st
 import numpy as np
 import pandas as pd
+from datetime import date, timedelta
 
-# -- Set page config
-apptitle = 'Doplnok dávky'
-
-st.set_page_config(page_title=apptitle, page_icon=":radioactive:")
-
-# Title the app
+st.set_page_config(page_title='Doplnok dávky', page_icon=":radioactive:")
 st.title('Doplnok dávky rádioterapie :radioactive:')
 
 st.sidebar.markdown("## Výber parametrov pre tumor/OARs:")
 
-ab_tumor = st.sidebar.number_input("$\\alpha/\\beta~pre~tumor$", min_value=0.1, max_value=20.0, value=10.0, step=0.1)
+ab_tumor = st.sidebar.number_input("$\\alpha/\\beta~pre~tumor$", min_value=0.1, max_value=20.0, value=10.0,
+                                   step=0.1)
 ab_org = st.sidebar.number_input("$\\alpha/\\beta~pre~OAR$", min_value=0.1, max_value=20.0, value=3.0, step=0.1)
+
 k = st.sidebar.number_input("$k~pre~tumor$", min_value=0.0, max_value=3.0, step=0.05, value=0.9,
                             help="Proliferačný parameter v Gy/d pre tumor. Pre OARs uvažujeme k=0.")
+
 t_delay = st.sidebar.number_input("$t_{delay}$", min_value=0, max_value=100, value=28,
                             help="Čas od začiatku terapie kedy nastupuje akcelerovaná repopulácia v dňoch.")
 
 st.sidebar.markdown("## Parametre predpísaného kurzu (ideál):")
+
 frakcia_pk = st.sidebar.number_input("dávka na frakciu", min_value=1, max_value=10, value=2)
+
 pocet_f_pk = st.sidebar.number_input("počet frakcií", min_value=1, max_value=50, value=27)
+
 pocet_dni_pk = st.sidebar.number_input("počet dní kurzu", min_value=1, max_value=365, value=37,
                                        help="Ideálna dĺžka predpísanej rádioterapie bez prerušení v dňoch.")
 
 st.markdown('####')
 
-# predpisana BED na organy
+# predpisana BED na organy (_pk znaci predpisany kurz, _rk znaci realny kurz)
 bed_org_pk = pocet_f_pk * frakcia_pk * (1 + frakcia_pk/ab_org)
 
 # predpisana BED na tumor
-
 if pocet_dni_pk > t_delay:
     bed_tumor_pk = pocet_f_pk * frakcia_pk * (1 + frakcia_pk/ab_tumor) - k * (pocet_dni_pk - t_delay)
 else:
@@ -53,26 +54,48 @@ st.markdown('####')
 
 st.markdown("#### Parametre odžiareného kurzu (reál):")
 
-cols = st.columns(2)
-with cols[0]:
-    pocet_f_pred_pauzou = st.number_input("počet frakcií pred pauzou", min_value=1, max_value=365, value=23)
-with cols[1]:
-    pocet_f_po_pauze = st.number_input("počet frakcií po pauze", min_value=0, max_value=50, value=4)
+cez_datumy = st.checkbox("Zaklikni, ak chceš zadať dĺžku kurzu podľa kalendára.")
+if cez_datumy:
+    cols = st.columns(2)
+    with cols[0]:
+        start_date = st.date_input('Dátum zahájenia terapie.', date(2025, 11, 26))
+    with cols[1]:
+        end_date = st.date_input('Dátum ukončenia rádioterapie.', date(2026, 1, 14))
+    pocet_dni_rk = (end_date - start_date + timedelta(days=1)).days
 
-cols = st.columns(2)
-with cols[0]:
-    pocet_pridanych_f = st.number_input("počet pridaných frakcií", min_value=0, max_value=50, value=2)
-with cols[1]:
-    pocet_dni_rk = st.number_input("celkový počet dní kurzu", min_value=1, max_value=365, value=50)
+    cols = st.columns(2)
+    with cols[0]:
+        pocet_f_pred_pauzou = st.number_input("počet frakcií pred pauzou", min_value=1, max_value=365, value=23)
+    with cols[1]:
+        pocet_f_po_pauze = st.number_input("počet frakcií po pauze", min_value=0, max_value=50, value=4)
 
-cols = st.columns(2)
-with cols[0]:
-    tumor_control = st.number_input("tumor control %", min_value=0, max_value=100, value=100)
-    tumor_control = tumor_control/100.0 # 1 <=> 100% prepisanej BED
+    cols = st.columns(2)
+    with cols[0]:
+        pocet_pridanych_f = st.number_input("počet pridaných frakcií", min_value=0, max_value=50, value=2)
+    with cols[1]:
+        tumor_control = st.number_input("tumor control %", min_value=0.1, max_value=100.0, value=100.0, step=0.1)
+        tumor_control = tumor_control / 100.0  # 1 <=> 100% prepisanej BED
+else:
+    cols = st.columns(2)
+    with cols[0]:
+        pocet_f_pred_pauzou = st.number_input("počet frakcií pred pauzou", min_value=1, max_value=365, value=23)
+    with cols[1]:
+        pocet_f_po_pauze = st.number_input("počet frakcií po pauze", min_value=0, max_value=50, value=4)
+
+    cols = st.columns(2)
+    with cols[0]:
+        pocet_pridanych_f = st.number_input("počet pridaných frakcií", min_value=0, max_value=50, value=2)
+    with cols[1]:
+        pocet_dni_rk = st.number_input("celkový počet dní kurzu", min_value=1, max_value=365, value=50)
+
+    cols = st.columns(2)
+    with cols[0]:
+        tumor_control = st.number_input("tumor control %", min_value=0.1, max_value=100.0, value=100.0, step=0.1)
+        tumor_control = tumor_control / 100.0  # 1 <=> 100% prepisanej BED
 
 bed_tumor_rk = bed_tumor_pk*tumor_control
 
-# doplnok
+# Vypocet navysenia doplnku
 bed_pred_pauzou_k0 = pocet_f_pred_pauzou * frakcia_pk * (1 + frakcia_pk/ab_tumor)
 if pocet_dni_rk > t_delay:
     utlm = k * (pocet_dni_rk - t_delay)
@@ -89,29 +112,36 @@ if np.isnan(doplnok_f):
 st.markdown('####')
 
 # navysena BED na organy
-bed_org_add = pocet_f_pred_pauzou * frakcia_pk * (1+frakcia_pk/ab_org) + (pocet_f_po_pauze + pocet_pridanych_f) * doplnok_f * (1+doplnok_f/ab_org)
+bed_org_add = (pocet_f_pred_pauzou * frakcia_pk * (1+frakcia_pk/ab_org) + (pocet_f_po_pauze + pocet_pridanych_f) *
+               doplnok_f * (1+doplnok_f/ab_org))
 bed_org_navyse_perc = ((bed_org_add/bed_org_pk)-1)*100
 
 # kontrola znizenie BED na tumor
 bed_tumor_ponize_perc = ((bed_tumor_rk/bed_tumor_pk)-1)*100
 
-st.metric(label="Navýšenie frakcií a doplnok po pauze",value=f"{pocet_f_po_pauze} x {doplnok_f:0.2f} + {pocet_pridanych_f} x "
-                                            f"{doplnok_f:0.2f} Gy")
+cols = st.columns(2)
+with cols[0]:
+    st.metric(label="Navýšenie frakcií a doplnok po pauze", value=f"{pocet_f_po_pauze} x {doplnok_f:0.2f} + "
+                                                                 f"{pocet_pridanych_f} x {doplnok_f:0.2f} Gy")
+with cols[1]:
+    st.metric(label="Počet dní reálneho kurzu rádioterapie.", value=pocet_dni_rk,
+              delta=f"{((pocet_dni_rk/pocet_dni_pk)-1)*100:0.1f}\%", delta_color="inverse")
 
 cols = st.columns(2)
 with cols[0]:
-    st.metric(label="Tumor BED s manažmentom prerušení",value=f"{bed_tumor_rk:0.2f} Gy_{ab_tumor}",
+    st.metric(label="Tumor BED s manažmentom prerušení", value=f"{bed_tumor_rk:0.2f} Gy_{ab_tumor}",
               delta=f"{bed_tumor_ponize_perc:0.1f}\%")
 with cols[1]:
-    bed_tumor_rk_bezm = pocet_f_pk * frakcia_pk * (1 + frakcia_pk/ab_tumor) - k * (pocet_dni_rk - pocet_pridanych_f - t_delay)
+    bed_tumor_rk_bezm = pocet_f_pk * frakcia_pk * (1 + frakcia_pk/ab_tumor) - k * (pocet_dni_rk - pocet_pridanych_f
+                                                                                   - t_delay)
     bed_tumor_ponize_bezm_perc = ((bed_tumor_rk_bezm/bed_tumor_pk)-1)*100
-    st.metric(label="Tumor BED bez manažmentu prerušení",value=f"{bed_tumor_rk_bezm:0.2f} Gy_{ab_tumor}",
+    st.metric(label="Tumor BED bez manažmentu prerušení", value=f"{bed_tumor_rk_bezm:0.2f} Gy_{ab_tumor}",
               delta=f"{bed_tumor_ponize_bezm_perc:0.1f}\%", help="Pre presný výpočet tejto metriky nastav $počet~pridaných~frakcií$ na 0 a $celkový~počet~dní~kurzu$ na reálny počet dní za ktorý (by) bol pôvodný predpísaný kurz odžiarený, " \
               "inak útlm dávky defaultne ráta s hodnotou $celkový~počet~dní~kurzu - počet~pridaných~frakcií$. Toto je dôležité pri prídavkoch, ktoré zasahujú do viacerých pracovných týždňov.")
 
 cols = st.columns(2)
 with cols[0]:
-    st.metric(label="OARs BED s manažmentom prerušení",value=f"{bed_org_add:0.2f} Gy_{ab_org}",
+    st.metric(label="OARs BED s manažmentom prerušení", value=f"{bed_org_add:0.2f} Gy_{ab_org}",
               delta=f"+{bed_org_navyse_perc:0.1f}\%", delta_color="inverse")
 
 st.markdown('#')
